3030a1f9858e18f18645a68311587812766f4e0e
jaaxxm <jaaxxm@gmail.com>
2013-07-21 13:57:56 +0300

    mix toggles added, artist page layout improved

--- app/scripts/controllers/mix.js
+++ app/scripts/controllers/mix.js
@@ -1,49 +1,52 @@
 'use strict';
 
 angular.module('samuraiFMApp')
-    .controller('MixCtrl', function ($scope) {
-        
-        $scope.statBlock = false;
-        $scope.favsBlock = false;
-        
-        $scope.toggleBlock = function(block, $event) {
-            $scope.block = true;
-            $event.preventDefault();
-        }
+.controller('MixCtrl', function ($scope) {
+  
+  $scope.state = 'hide';
 
+  $scope.statBlock = false;
+  $scope.favsBlock = false;
 
-        $scope.isCollapsed = true;
+  $scope.toggleStat = function() {  
+    $scope.statBlock = !($scope.statBlock);
+  }
+  $scope.toggleFavs = function() {  
+    $scope.favsBlock = !($scope.favsBlock);
+  }
 
-        $scope.shouts = [
-            {
-                member: 'Archibal',
-                timestamp: '00:02',
-                comment: 'sublime'
-            },
-            {
-                member: 'garix',
-                timestamp: '00:12',
-                comment: 'old school // ... it\'s a pleasure!'
-            },
-            {
-                member: 'aaguirre',
-                timestamp: '16:15',
-                comment: 'relaxing :)'
-            },
-            {
-                member: 'synergist',
-                timestamp: '21:15',
-                comment: 'beautiful!'
-            },
-            {
-                member: 'olulo original',
-                timestamp: '22:15',
-                comment: 'Truly nice!'
-            },
-            {
-                member: 'Mark Hjorthoy',
-                timestamp: '40:02',
-                comment: 'Fantastic! What agreat mix of music gentlemen!'
-            }
-        ];
-    });
+  $scope.isCollapsed = true;
+
+  $scope.shouts = [
+    {
+      member: 'Archibal',
+      timestamp: '00:02',
+      comment: 'sublime'
+    },
+    {
+      member: 'garix',
+      timestamp: '00:12',
+      comment: 'old school // ... it\'s a pleasure!'
+    },
+    {
+      member: 'aaguirre',
+      timestamp: '16:15',
+      comment: 'relaxing :)'
+    },
+    {
+      member: 'synergist',
+      timestamp: '21:15',
+      comment: 'beautiful!'
+    },
+    {
+      member: 'olulo original',
+      timestamp: '22:15',
+      comment: 'Truly nice!'
+    },
+    {
+      member: 'Mark Hjorthoy',
+      timestamp: '40:02',
+      comment: 'Fantastic! What agreat mix of music gentlemen!'
+    }
+  ];
+});
