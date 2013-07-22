a36d08e379f07fc4ddff7dafd6ca85a984ee6d7c
jaaxxm <jaaxxm@gmail.com>
2013-07-23 00:21:15 +0300

    artist: tabs, comments removed, followers moved to the top; mix: artist column adjusted

--- app/scripts/controllers/artist.js
+++ app/scripts/controllers/artist.js
@@ -2,9 +2,5 @@
 
 angular.module('samuraiFMApp')
   .controller('ArtistCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+		$scope.isCollapsed = true;
   });
